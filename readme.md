be481518d9e2d21f6569b7445d525aabd4c6ac7c
brain.duan <brain.duan@analyticservice.net>
2021-10-12 16:01:56 +0800

    add dgraph findany findsome

---
+++ readme.md
@@ -0,0 +1,5 @@
+
+Schema 
+```
+curl 'http://127.0.0.1:8080/alter'   -H 'Connection: keep-alive'   -H 'Pragma: no-cache'   -H 'Content-Type: text/plain;charset=UTF-8'  -H 'Accept: */*'     --data-raw '<xid>: string @index(hash) .'   --compressed
+```
--- src/main/java/aas/dgraph/jena/client/DgraphService.java
+++ src/main/java/aas/dgraph/jena/client/DgraphService.java
@@ -8,12 +8,19 @@ import io.dgraph.Transaction;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.apache.jena.atlas.iterator.Iter;
+import org.apache.jena.atlas.iterator.IteratorWithBuffer;
+import org.apache.jena.atlas.json.JSON;
+import org.apache.jena.atlas.json.JsonObject;
 import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
+import org.apache.jena.rdfconnection.RDFConnectionLocal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Iterator;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class DgraphService {
     private static final Logger logger = LoggerFactory.getLogger(DgraphService.class);
@@ -41,29 +48,34 @@ public class DgraphService {
         logger.info("{}", rdfs);
     }
 
-    public void schema() {
+    public List<String> predicates() {
         Transaction rt = client.newReadOnlyTransaction();
-        DgraphProto.Response resp = rt.queryRDF("schema{}");
-        String json = resp.getJson().toStringUtf8();
-        logger.info("{}", json);
+        DgraphProto.Response resp = rt.query("schema{}");
+        String jsonStr = resp.getJson().toStringUtf8();
+        JsonObject json = JSON.parse(jsonStr);
+        List<String> preds = json.getArray("schema")
+                .map(x -> x.getAsObject().get("predicate").getAsString().value())
+                .filter(s -> !s.startsWith("dgraph."))
+                .map(Gql::wellFormatPredict)
+                .collect(Collectors.toList());
+        rt.close();
+        return preds;
     }
 
     public static void main(String[] args) {
         DgraphService ds = new DgraphService("localhost:9080");
         ds.queryRdf();
+        ds.predicates();
     }
 
     public void add(Node s, Node p, Node o) {
         try {
-            logger.info("SPO Name {} {} {}", s.toString(), p.toString(), o.toString());
             Transaction trans = client.newTransaction();
             String xidQuery = Gql.render(Gql.load("gql/xid.gql"), "subject", s.toString());
-            logger.info("xid Query: {}", xidQuery);
             String setQuery = Gql.render(Gql.load("gql/setWithXid.gql"),
                     "subject", Gql.wellFormatValue(s.toString()),
                     "predict", Gql.wellFormatPredict(p.toString()),
                     "object", Gql.wellFormatValue(o.toString()));
-            logger.info("set Query: {}", setQuery);
 
             trans.doRequest(DgraphProto.Request.newBuilder()
                     .setCommitNow(true)
@@ -78,6 +90,78 @@ public class DgraphService {
     }
 
     public Iterator<Triple> find(Node s, Node p, Node o) {
-        return Iter.nullIterator();
+        if (s == Node.ANY) {
+            return findAny(p, o);
+        } else {
+            return findSome(s, p, o);
+        }
+    }
+
+    private Iterator<Triple> findSome(Node s, Node p, Node o) {
+        List<String> pred = predicates();
+        String fields = pred.stream().collect(Collectors.joining(" \n"));
+        String gql = Gql.render(Gql.load("gql/findSome.gql"),
+                "preds", fields, "xid", cleanString(s.toString()));
+        Transaction rot = client.newReadOnlyTransaction();
+        DgraphProto.Response resp = rot.queryRDF(gql);
+        String rdfs = resp.getRdf().toStringUtf8();
+//        logger.info("{}", rdfs);
+
+        return rdfStringToTriples(rdfs);
+    }
+
+    private Iterator<Triple> findAny(Node p, Node o) {
+        List<String> pred = predicates();
+        String fields = pred.stream().collect(Collectors.joining(" \n"));
+        String gql = Gql.render(Gql.load("gql/findAny.gql"), "preds", fields);
+        Transaction rot = client.newReadOnlyTransaction();
+        DgraphProto.Response resp = rot.queryRDF(gql);
+        String rdfs = resp.getRdf().toStringUtf8();
+        logger.info("{}", rdfs);
+        return rdfStringToTriples(rdfs);
+    }
+
+    private Iterator<Triple> rdfStringToTriples(String rdfstr) {
+        logger.info("RDFs {}", rdfstr);
+        if (rdfstr.isEmpty()) {
+            return Iter.nullIter();
+        }
+        String[] rdfs = rdfstr.split("\n");
+        Map<String, String> cache = new ConcurrentHashMap<>();
+
+        Arrays.stream(rdfs).filter(line -> line.contains("<xid>")).forEach(
+                line -> {
+                    String[] arr = line.split("<xid>");
+                    cache.put(arr[0].trim(), arr[1].replace(" .", "").trim());
+                }
+        );
+        List<Triple> ret = new LinkedList<>();
+        Arrays.stream(rdfs).filter(line -> !line.contains("<xid>")).forEach(line -> {
+            String[] arr = line.split("> ");
+            String sub = cleanString(cache.get(arr[0] + ">"));
+            String pre = cleanString(arr[1] + ">");
+            String obj = cleanString(arr[2].replace(" .", ""));
+            Triple t = new Triple(
+                    NodeFactory.createLiteral(sub),
+                    NodeFactory.createURI(pre),
+                    NodeFactory.createLiteral(obj)
+            );
+            ret.add(t);
+        });
+//        logger.info("{}", ret);
+        return ret.iterator();
+    }
+
+    private String cleanString(String s) {
+        if (s == null) {
+            return "";
+        }
+        if (s.startsWith("\"") && s.endsWith("\"")) {
+            return s.substring(1, s.length() - 1);
+        }
+        if (s.startsWith("<") && s.endsWith(">")) {
+            return s.substring(1, s.length() - 1);
+        }
+        return s;
     }
 }
